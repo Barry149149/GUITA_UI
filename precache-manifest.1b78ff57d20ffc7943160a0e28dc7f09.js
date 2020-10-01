b4a233d3ebe061f744f36ec4f42f47b13a5ed3c1
unknown <barry149149@gmail.com>
2020-10-01 15:53:27 +0800

    Updates

---
+++ precache-manifest.1b78ff57d20ffc7943160a0e28dc7f09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118cc75fa1a8258fdb0b89fae6f9f2fa",
+    "url": "/GUITA_UI/index.html"
+  },
+  {
+    "revision": "2b1b61bb96645791f735",
+    "url": "/GUITA_UI/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5474ed78aac37f3d13d0",
+    "url": "/GUITA_UI/static/js/2.f11e82bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GUITA_UI/static/js/2.f11e82bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1b61bb96645791f735",
+    "url": "/GUITA_UI/static/js/main.995a6090.chunk.js"
+  },
+  {
+    "revision": "4a8694b27cfece9d5436",
+    "url": "/GUITA_UI/static/js/runtime-main.e958554c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GUITA_UI/static/media/logo.5d5d9eef.svg"
+  }
+]);
