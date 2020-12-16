a321800d8553714b3be6799787c32d7cd27bd052
unknown <barry149149@gmail.com>
2020-12-16 15:31:16 +0800

    Selection bars are added

---
+++ src/tab/selectionBars/DriverSelect.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import { driverOption } from './docs/data';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+export default function DriverSelect() {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        framework:''
+    });
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        setState({
+            ...state,
+            [name]: event.target.value,
+        });
+    };
+    return(
+        <div>
+            <FormControl className={classes.formControl}>
+                <InputLabel shrink htmlFor="age-native-label-placeholder">
+                    Driver
+                </InputLabel>
+                <NativeSelect
+                    value={state.age}
+                    onChange={handleChange}
+                    inputProps={{
+                        name: 'age',
+                        id: 'age-native-label-placeholder',
+                    }}
+                >
+                    {driverOption.map(({index,value,label}) => {
+                        return (
+                            <option key={index} value={value}>
+                                {label}
+                            </option>
+                        )
+                    })
+                    }
+                </NativeSelect>
+                <FormHelperText>Select the driver for the assignment</FormHelperText>
+            </FormControl>
+        </div>
+    )
+}
