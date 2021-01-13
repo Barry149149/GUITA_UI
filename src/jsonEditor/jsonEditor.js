3791e1b239c52879c167dd197f1da059b1c35543
Chan Chung Yin <barry149149@gmail.com>
2021-01-13 18:41:46 +0800

    conflict

--- src/jsonEditor/jsonEditor.js
+++ src/jsonEditor/jsonEditor.js
@@ -3,7 +3,7 @@ import locale from "react-json-editor-ajrm/locale/en";
 import Box from "@material-ui/core/Box";
 import UploadFiles from "../components/upload-files.component";
 import Container from "@material-ui/core/Container";
-import React, {useEffect} from "react";
+import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
