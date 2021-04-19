184145929c337a82b24f39b6b4a201a10dd61118
Jeanie Lam <jeanielamwh@gmail.com>
2021-04-19 20:13:00 +0800

    Tidy up the buttons under test case tree

--- src/components/tab/tabpanels/drawerPanels/TreePanel.js
+++ src/components/tab/tabpanels/drawerPanels/TreePanel.js
@@ -10,6 +10,9 @@ import { saveAs } from 'file-saver'
 import { Prompt, Route, useParams } from 'react-router-dom'
 import NavigationBlocker from '../../../Prompt/Warning'
 import { commandDescription } from '../../../../docs/commandList'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
+import CloudUploadIcon from '@material-ui/icons/CloudUpload'
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload'
 
 const useStyles = makeStyles((theme) => ({
   readOnlyBox: {
@@ -19,8 +22,42 @@ const useStyles = makeStyles((theme) => ({
   },
   uploadButton: {
     height: 40,
-    overflowX: 'hidden',
-    overflow: 'hidden'
+    // overflowX: 'hidden',
+    // overflow: 'hidden',
+    textTransform: 'capitalize',
+    // fontWeight: "bold",
+    fontSize: '14px',
+    minWidth: 220,
+    backgroundColor: 'ghostwhite'
+    // color: "white"
+    // border: "2px solid"
+  },
+  tooltip: {
+    color: 'lightgray',
+    marginLeft: 20
+  },
+  tooltipText: {
+    // color: "red"
+    padding: '8px',
+    fontSize: '14px',
+    lineHeight: '1.5em'
+  },
+  buttonWithTooltip: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  buttonGroup: {
+    height: 200,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  panelBody: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '100%'
   }
 }))
 
@@ -30,10 +67,11 @@ export default function TreePanel(props) {
   let { assignment_id, assignment_name } = useParams()
 
   return (
-    <React.Fragment>
-      <Box p={3}>
-        <Title>Test Cases</Title>
-        <Divider />
+    // Case tree width is 260
+    <Box p={3} style={{ width: 260, height: '90%' }}>
+      <Title>Test Cases</Title>
+      <Divider />
+      <div className={classes.panelBody}>
         <CaseTree
           selectedCase={props.selectedCase}
           tree={props.tree}
@@ -44,157 +82,176 @@ export default function TreePanel(props) {
           setDeletedTestcase={props.setDeletedTestcase}
           deletedTestcase={props.deletedTestcase}
         />
-        <Box pt={3} />
-        <div
-          style={{
-            position: 'absolute',
-            bottom: 30,
-            left: 150
-          }}>
-          <Button
-            className={classes.uploadButton}
-            id="button_upload"
-            variant="outlined"
-            component="label"
-            color="primary"
-            onClick={() => {}}>
-            Upload Image file
-            <input
-              type="file"
-              id="file"
-              name="file"
-              accept="application/octet-stream,application/zip-compressed,application/x-zip,application/x-zip-compressed"
-              hidden
-              onChange={(e) => {
-                //TODO: submit img zip
-                props.setFile({
-                  ...props.file,
-                  zip_filename: e.target.files[0].name,
-                  zip: e.target.files[0]
-                })
-                e.target.value = null
-              }}
-            />
-          </Button>
-          <pr style={{ fontSize: 14, color: '#666666' }}>
-            {' '}
-            {props.file.zip_filename
-              ? props.file.zip_filename
-              : 'No file selected'}
-          </pr>
-          <p style={{ fontSize: 12, color: '#888888' }}>
-            Upload a ZIP of png files for Image Comparisons
-          </p>
-          <Box pt={1} />
-          {
-            // TODO: not applicable format
-          }
-          <Button
-            size="small"
-            id="button_upload"
-            variant="outlined"
-            component="label"
-            color="primary"
-            className={classes.uploadButton}
-            onClick={() => {}}>
-            Upload test cases file
-            <input
-              type="file"
-              id="file"
-              name="file"
-              accept="application/octet-stream,application/zip-compressed,application/x-zip,application/x-zip-compressed"
-              // TODO: Chrome accept also i.e. pptx, docx, xlsx, other browsers work fine
-              hidden
-              onChange={(e) => {
-                const promises = []
+        <div className={classes.buttonGroup}>
+          <div>
+            <div style={{ fontSize: 14, fontStyle: 'italic', padding: '6px' }}>
+              {props.file.zip_filename ? (
+                <div style={{ color: '#3f51b3' }}>
+                  {props.file.zip_filename}
+                </div>
+              ) : (
+                <div style={{ color: 'lightgray' }}>No file uploaded</div>
+              )}
+            </div>
+            <div className={classes.buttonWithTooltip}>
+              <Button
+                className={classes.uploadButton}
+                id="button_upload"
+                variant="outlined"
+                component="label"
+                color="primary"
+                // startIcon={<CloudUploadIcon/>}
+                onClick={() => {}}>
+                Upload Image File
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  accept="application/octet-stream,application/zip-compressed,application/x-zip,application/x-zip-compressed"
+                  hidden
+                  onChange={(e) => {
+                    //TODO: submit img zip
+                    props.setFile({
+                      ...props.file,
+                      zip_filename: e.target.files[0].name,
+                      zip: e.target.files[0]
+                    })
+                    e.target.value = null
+                  }}
+                />
+              </Button>
+              <Tooltip
+                className={classes.tooltip}
+                title={
+                  <div className={classes.tooltipText}>
+                    Upload a ZIP of .png files for image comparisons
+                  </div>
+                }>
+                <HelpOutlineIcon />
+              </Tooltip>
+            </div>
+          </div>
 
-                JSZip.loadAsync(e.target.files[0]).then(function (zip) {
-                  zip.forEach(function (relativePath, zipEntry) {
-                    promises.push(zip.file(zipEntry.name).async('string'))
-                  })
+          {/*<Box pt={1} />*/}
+          {/*{*/}
+          {/*  // TODO: not applicable format*/}
+          {/*}*/}
+          <div className={classes.buttonWithTooltip}>
+            <Button
+              // size="small"
+              id="button_upload"
+              variant="outlined"
+              component="label"
+              color="primary"
+              // startIcon={<CloudUploadIcon/>}
+              className={classes.uploadButton}
+              onClick={() => {}}>
+              Upload test cases file
+              <input
+                type="file"
+                id="file"
+                name="file"
+                accept="application/octet-stream,application/zip-compressed,application/x-zip,application/x-zip-compressed"
+                // TODO: Chrome accept also i.e. pptx, docx, xlsx, other browsers work fine
+                hidden
+                onChange={(e) => {
+                  const promises = []
 
-                  Promise.all(promises).then(
-                    function (data) {
-                      let newNodes = [...props.tree[0].nodes]
-                      console.log(data.length)
-                      console.log(props.createdCases)
-                      let last_jsObject = []
-                      let last_new_json_id = []
-                      let last_id = 0
+                  JSZip.loadAsync(e.target.files[0]).then(function (zip) {
+                    zip.forEach(function (relativePath, zipEntry) {
+                      promises.push(zip.file(zipEntry.name).async('string'))
+                    })
 
-                      for (const i in data) {
-                        const jsObject = JSON.parse(data[i])
-                        console.log(jsObject)
+                    Promise.all(promises).then(
+                      function (data) {
+                        let newNodes = [...props.tree[0].nodes]
+                        console.log(data.length)
+                        console.log(props.createdCases)
+                        let last_jsObject = []
+                        let last_new_json_id = []
+                        let last_id = 0
 
-                        let new_json_id = []
-                        let new_json = []
-                        for (let j = 0; j < jsObject.length; j++) {
-                          let tempDescription =
-                            typeof jsObject[i].description === 'undefined'
-                              ? commandDescription(jsObject[j])
-                              : jsObject[i].description
-                          new_json.push({
-                            ...jsObject[i],
-                            description: tempDescription
-                          })
-                          new_json_id.push({
-                            id: j + 1,
-                            command: {
-                              ...jsObject[j],
+                        for (const i in data) {
+                          const jsObject = JSON.parse(data[i])
+                          console.log(jsObject)
+
+                          let new_json_id = []
+                          let new_json = []
+                          for (let j = 0; j < jsObject.length; j++) {
+                            let tempDescription =
+                              typeof jsObject[i].description === 'undefined'
+                                ? commandDescription(jsObject[j])
+                                : jsObject[i].description
+                            new_json.push({
+                              ...jsObject[i],
                               description: tempDescription
-                            }
-                          })
-                        }
-
-                        newNodes.push({
-                          id: props.createdCases + parseInt(i) + 1,
-                          value:
-                            'Test ' + (props.createdCases + parseInt(i) + 1),
-                          json: new_json,
-                          json_id: new_json_id
-                        })
-
-                        last_id = i
-                        last_jsObject = new_json
-                        last_new_json_id = new_json_id
-                      }
-
-                      props.dispatch({
-                        data: {
-                          tree: [
-                            {
-                              value: 'Test Cases',
-                              nodes: newNodes
-                            }
-                          ],
-                          createdCases: props.createdCases + data.length,
-                          noOfCases: props.createdCases + data.length,
-                          selectedCase: {
-                            id: last_id,
-                            json: last_jsObject,
-                            json_id: last_new_json_id
+                            })
+                            new_json_id.push({
+                              id: j + 1,
+                              command: {
+                                ...jsObject[j],
+                                description: tempDescription
+                              }
+                            })
                           }
+
+                          newNodes.push({
+                            id: props.createdCases + parseInt(i) + 1,
+                            value:
+                              'Test ' + (props.createdCases + parseInt(i) + 1),
+                            json: new_json,
+                            json_id: new_json_id
+                          })
+
+                          last_id = i
+                          last_jsObject = new_json
+                          last_new_json_id = new_json_id
                         }
-                      })
-                    },
-                    function (err) {}
-                  )
-                })
-                e.target.value = null
-              }}
-            />
-          </Button>
-          <p style={{ fontSize: 12, color: '#888888' }}>
-            All test case file should placed under the root of ZIP
-          </p>
-          <Divider />
-          <Box pt={1} />
+
+                        props.dispatch({
+                          data: {
+                            tree: [
+                              {
+                                value: 'Test Cases',
+                                nodes: newNodes
+                              }
+                            ],
+                            createdCases: props.createdCases + data.length,
+                            noOfCases: props.createdCases + data.length,
+                            selectedCase: {
+                              id: last_id,
+                              json: last_jsObject,
+                              json_id: last_new_json_id
+                            }
+                          }
+                        })
+                      },
+                      function (err) {}
+                    )
+                  })
+                  e.target.value = null
+                }}
+              />
+            </Button>
+            <Tooltip
+              className={classes.tooltip}
+              title={
+                <div className={classes.tooltipText}>
+                  All test case files should be placed under the root of the ZIP
+                </div>
+              }>
+              <HelpOutlineIcon />
+            </Tooltip>
+          </div>
+          <Divider variant="fullWidth" />
+
           <Button
             className={classes.uploadButton}
-            size="small"
+            // size="small"
             id="button_download"
             variant="outlined"
             color="primary"
+            // startIcon={<CloudDownloadIcon/>}
             onClick={() => {
               const zip = new JSZip()
               console.log(props.tree[0].nodes[0].json)
@@ -209,7 +266,7 @@ export default function TreePanel(props) {
             Download Test Cases File
           </Button>
         </div>
-      </Box>
-    </React.Fragment>
+      </div>
+    </Box>
   )
 }
